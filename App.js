b1435fbbe683315fbb8c46ced74c20633a9ccc7f
Kevin <kevin@myplaceonline.com>
2021-01-11 23:26:56 -0800

    Use SplashScreen.preventAutoHideAsync

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
-  return (
-    <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    appIsReady: false,
+  };
+  
+  async componentDidMount() {
+    // Prevent native splash screen from autohiding
+    try {
+      console.log("Calling SplashScreen.preventAutoHideAsync");
+      await SplashScreen.preventAutoHideAsync();
+      console.log("Calling setTimeout");
+      setTimeout(this.slowSplashCallback, 5000);
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+  slowSplashCallback = async () => {
+    console.log("setTimeout finished");
+    this.setState({ appIsReady: true }, async () => {
+      await SplashScreen.hideAsync();
+    });
+  }
+
+  render() {
+    if (!this.state.appIsReady) {
+      return null;
+    }
+
+    return (
+      <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}>
+        <Text>testsplash</Text>
+      </View>
+    );
+  }
 }
